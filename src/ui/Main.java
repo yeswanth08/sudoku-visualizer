f4e9413fdb05d0fb2cd846c6aea26592bc554542
yeswanth08 <curiousyeswanth003@gmail.com>
2024-07-11 14:29:39 +0530

    initial commit

---
+++ src/ui/Main.java
@@ -0,0 +1,7 @@
+import utils.Frame;
+
+public class Main {
+    public static void main(String[] args) {
+        Frame frame  = new Frame();
+    }
+}
---
+++ src/ui/utils/customfeatures.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class customfeatures{
+    public String Title;
+    public customfeatures(){
+        this.Title =  "Sudoku-Visualizer";
+    }
+}
